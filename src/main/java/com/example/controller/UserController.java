c0b5c50bdbd136d3c3e2fcf23b74e0d5ee362305
ttekesuke <tekesuke1986@hotmail.com>
2019-01-06 22:50:17 +0900

    CORS用、不要メソッド削除

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -12,7 +12,7 @@ import java.util.List;
 /**
  * Created by ishiis on 16/09/08.
  */
-
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -34,13 +34,6 @@ public class UserController {
     public User createUser(@Validated @RequestBody User user) {
         return userService.save(user);
     }
-
-    @RequestMapping(method=RequestMethod.PUT, value="{id}")
-    public User updateUser(@PathVariable("id") Long id, @RequestBody User user) {
-        user.setId(id);
-        return userService.save(user);
-    }
-
     @RequestMapping(method=RequestMethod.DELETE, value="{id}")
     public void deleteUser(@PathVariable("id") Long id) {
         userService.delete(id);
--- src/main/java/com/example/entity/User.java
+++ src/main/java/com/example/entity/User.java
@@ -15,4 +15,5 @@ public class User {
     private Long id;
     private String name;
     private String email;
+    private String password;
 }
